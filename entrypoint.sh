8a81391fdfe12500b8fc73d36182130e0e883dc4
ju5t <ju5t@users.noreply.github.com>
2023-08-26 16:23:21 +0200

    fix: make all fields optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
 if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+  then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
 if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+  then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+if [ ! -z $INPUT_SHELL ];
+  then INPUT_OPTIONS="$INPUT_OPTIONS --entrypoint $INPUT_SHELL"
+fi
+
+if [ ! -z $INPUT_RUN ];
+   then INPUT_OPTIONS="$INPUT_OPTIONS -c ${INPUT_RUN//$'\n'/;}"
+fi
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS
